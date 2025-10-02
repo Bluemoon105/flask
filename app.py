d77aa70ce0d9ed367a7cb3ffae644749c4426f62
Bluemoon105 <lyj69641@gmail.com>
2025-10-02 11:27:41 +0900

    env 설정

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from db import get_db_connection
+import os
+from dotenv import load_dotenv
+
+# .env 파일 읽기
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", name="name")
+    conn = get_db_connection()
+    if conn:
+        conn.close()
+    return render_template("index.html", name="예지")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
+    app.run(debug=debug_mode)
